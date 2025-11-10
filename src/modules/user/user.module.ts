c5f91af2aff154051b255a9435d28e247acf24e6
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-11-11 02:01:57 +0500

    dssd

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,11 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { User } from "./models";
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
+import { TelegramService } from "common/telegram";
 
 @Module({
     imports: [SequelizeModule.forFeature([User])],
-    providers: [UserService],
+    providers: [UserService,TelegramService],
     controllers: [UserController]
 })
 
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,10 +3,14 @@ import { InjectModel } from '@nestjs/sequelize';
 import { User } from './models/user.model';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { TelegramService } from '../../../common/telegram'; // ✅ qo‘shildi
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User) private userModel: typeof User) {}
+  constructor(
+    @InjectModel(User) private userModel: typeof User,
+    private readonly telegramService: TelegramService, // ✅ inject qilindi
+  ) {}
 
   async getAllUsers(): Promise<User[]> {
     return this.userModel.findAll();
@@ -18,11 +22,14 @@ export class UserService {
     return user;
   }
 
- async createUser(payload: CreateUserDto): Promise<User> {
-  const newUser = await this.userModel.create({ ...payload });
-  return newUser;
-}
+  async createUser(payload: CreateUserDto): Promise<User> {
+    const newUser = await this.userModel.create({ ...payload });
 
+    // ✅ Telegramga xabar yuborish
+    await this.telegramService.sendUserCreatedNotification(newUser);
+
+    return newUser;
+  }
 
   async updateUser(id: number, payload: UpdateUserDto): Promise<User> {
     const user = await this.userModel.findByPk(id);
